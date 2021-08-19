cc301c790262fd0459e6895762a902fdc83ed695
wpangestu <wa.pangestu16@gmail.com>
2021-08-20 01:53:56 +0700

    main feature is done

--- app/Controllers/ZipController.php
+++ app/Controllers/ZipController.php
@@ -3,11 +3,58 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use ZipArchive;
 
 class ZipController extends BaseController
 {
 	public function index()
 	{
 		//
+		try {
+			//code...
+			$zip = new ZipArchive();
+			$filename = "test112.zip";
+			
+			if ($zip->open($filename, ZipArchive::CREATE)!==TRUE) {
+				exit("cannot open <$filename>\n");
+			}
+			
+			// create a file and pass the content to second parameter
+			$zip->addFromString("tes_file.txt", "Halo World Wpangestu");
+			
+			// Add file from localrootdirectory and rename it
+			$zip->addFile(FCPATH."index.php","enjoy.php");
+
+			
+			$zip->close();
+
+			// var_dump($zip);die();
+
+			$file = FCPATH.$filename;
+			// die($file);
+			//Then download the zipped file.
+			header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename='.basename($file));
+            header('Content-Transfer-Encoding: binary');
+            header('Expires: 0');
+            header('Cache-Control: private');
+            header('Pragma: private');
+            header('Content-Length: ' . filesize($file));
+            ob_clean();
+            flush();
+            readfile($file);
+			unlink($file);
+            exit;
+
+		} catch (\Throwable $th) {
+			//throw $th;
+			d($th);			
+		}
+
+	}
+
+	public function generateZip(){
+
 	}
 }
